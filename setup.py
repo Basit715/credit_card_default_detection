082537255cae2250d54d36d89e40d1494a12ed33
Basit715 <basitabass27411@gmail.com>
2024-03-19 14:50:50 +0530

    created entire folder structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+     name = "Credit_Card_Default_Detection",
+     version="0.0.1",
+     author = "Basit pushoo",
+     author_email="basitabass27411@gmail.com",
+     packages=find_packages()
+) 
